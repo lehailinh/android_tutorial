87ca1e0a72b045ff5ee93bf96255ed37010bbb8e
LinhLH7 <v.linhlh7@vinsmart.net.vn>
2023-06-22 22:13:35 +0700

    testGit

---
+++ app/src/main/java/com/example/androidtutorial/Gittest.java
@@ -0,0 +1,32 @@
+package com.example.androidtutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class Gittest extends AppCompatActivity {
+    private EditText mOpenLink;
+    private static final String TAG = "Gitttest";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gittest);
+        mOpenLink = findViewById(R.id.url_link);
+    }
+
+    public void Webopen(View view) {
+    String urlLink = mOpenLink.getText().toString();
+    Uri webpage = Uri.parse(urlLink);
+    Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+    if(intent.resolveActivity(getPackageManager()) !=null){
+        startActivity(intent);
+    }else{
+        Log.d(TAG, "Open webpage fail!");
+    }
+    }
+}
